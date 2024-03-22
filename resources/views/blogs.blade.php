620de173313dd2afaab5daa3cd99b483c2359434
ashein-codethedreams <heroglasses155@gmail.com>
2024-03-22 14:18:39 +0630

    matter data parse

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -10,7 +10,12 @@
 
 <body>
     <?php foreach ($blogs as $blog) :?>
-    <?= $blog ?>
+    <h1><a href="blogs/<?= $blog->slug ?>"><?= $blog->title ?></a></h1>
+    <div>
+        <p>
+            <?= $blog->intro ?>
+        </p>
+    </div>
     <?php endforeach;?>
 </body>
 
