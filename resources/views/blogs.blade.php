7b84f733938447210b0b7d1df25b7cb6278a9c0e
ashein-codethedreams <heroglasses155@gmail.com>
2024-03-22 15:04:56 +0630

    replaced with blade syntax

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -10,11 +10,11 @@
 
 <body>
     <?php foreach ($blogs as $blog) :?>
-    <h1><a href="blogs/<?= $blog->slug ?>"><?= $blog->title ?></a></h1>
+    <h1><a href="blogs/{{ $blog->slug }}">{{ $blog->title }} </a></h1>
     <div>
-        <p> published at - <?= $blog->date ?></p>
+        <p> published at -{{ $blog->date }} </p>
         <p>
-            <?= $blog->intro ?>
+            {{ $blog->intro }}
         </p>
     </div>
     <?php endforeach;?>
