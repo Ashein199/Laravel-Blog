63d82eab0833b91acf9fc4dd03f15f6a9d36e5e1
ashein-codethedreams <heroglasses155@gmail.com>
2024-03-25 12:00:21 +0630

    split navbar component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,11 +9,7 @@
 </head>
 
 <body>
-    <nav>
-        <li><a href="">home</a></li>
-        <li><a href="">about</a></li>
-        <li><a href="">contact</a></li>
-    </nav>
+    <x-navbar />
     {{ $slot }}
 </body>
 
